301be1ff2d002e1c97e89b1852f507cbed4e62c4
marqos12 <marqos111222@gmail.com>
2019-05-02 21:15:05 +0200

    Dodanie logowania i rejestracji

--- Quiz/app/src/main/java/studia/quiz/LoginScreen.java
+++ Quiz/app/src/main/java/studia/quiz/LoginScreen.java
@@ -1,26 +1,73 @@
 package studia.quiz;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.gson.Gson;
+import com.squareup.okhttp.MediaType;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Protocol;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import studia.quiz.model.Answer;
+import studia.quiz.model.Question;
+import studia.quiz.model.User;
 
 public class LoginScreen extends AppCompatActivity {
-
+    String loginURL = "http://marqos12.000webhostapp.com/api/login";
+    MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+    Gson gson = new Gson();
+TextView loginFailed;
+TextView inProgress;
+    public ProgressDialog progress;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
 
-
+        final EditText login = findViewById(R.id.login);
+        final EditText password = findViewById(R.id.password);
+        loginFailed = findViewById(R.id.loginFailedText);
+        inProgress = findViewById(R.id.textInProgress);
         Button button3 = findViewById(R.id.loginBtn);
         button3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                login.setBackgroundColor(getResources().getColor(R.color.buttonBackgroundDisable));
+                password.setBackgroundColor(getResources().getColor(R.color.buttonBackgroundDisable));
 
-                //TODO: logowanie
-
+                if(TextUtils.isEmpty(login.getText())||login.getText().toString().trim().length() == 0){
+                   login.setBackgroundColor(getResources().getColor(R.color.textNegative));
+                }
+                else if(password.getText().equals("")||password.getText().toString().trim().length() == 0){
+                    password.setBackgroundColor(getResources().getColor(R.color.textNegative));
+                }
+                else {
+                    User user = new User();
+                    user.setEmail(login.getText().toString());
+                    user.setPassword(password.getText().toString());
+                    new LogIn().execute(user);
+                    loginFailed.setText("");
+                    inProgress.setVisibility(View.VISIBLE);
+                }
             }
         });
         Button button4 = findViewById(R.id.registerBtn);
@@ -33,4 +80,49 @@ public class LoginScreen extends AppCompatActivity {
         });
 
     }
+
+    private class LogIn extends AsyncTask<User, Void, String> {
+        private OkHttpClient mClient = new OkHttpClient();
+
+        protected String doInBackground(User... user){
+
+            RequestBody requestBody = RequestBody.create(JSON, gson.toJson(user[0]));
+
+            try {
+                com.squareup.okhttp.Request request = new Request
+                        .Builder()
+                        .post(requestBody)
+                        .url(loginURL)
+                        .build();
+                mClient.setProtocols(Arrays.asList(Protocol.HTTP_1_1));
+
+                Response response = mClient.newCall(request).execute();
+                String stringResponse = response.body().string();
+                return stringResponse;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+
+        protected void onPostExecute(String userResult) {
+            inProgress.setVisibility(View.INVISIBLE);
+            try {
+                JSONObject jsonObject = new JSONObject(userResult);
+                if(jsonObject.has("error")){
+                    loginFailed.setText("Logowanie nie powiodło się");
+                }
+                else {
+                    JSONObject userJsonObject = jsonObject.getJSONObject("user");
+                    User respUsser = new User(userJsonObject);
+
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
 }
---
+++ Quiz/app/src/main/java/studia/quiz/model/User.java
@@ -0,0 +1,100 @@
+package studia.quiz.model;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class User {
+    private Integer id;
+    private String name;
+    private String surname;
+    private String username;
+    private String email;
+    private String password;
+    private String role;
+    private String course;
+
+    private String c_password;
+
+    public User(JSONObject jsonObject) throws JSONException {
+        id = (Integer) jsonObject.get("id");
+        name = jsonObject.getString("name");
+        surname = jsonObject.getString("surname");
+        role = jsonObject.getString("role");
+        course = jsonObject.getString("course");
+        email = jsonObject.getString("email");
+    }
+
+    public User(){};
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public String getC_password() {
+        return c_password;
+    }
+
+    public void setC_password(String c_password) {
+        this.c_password = c_password;
+    }
+}
